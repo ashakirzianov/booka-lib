592512ce80216f0151d07a05819aabb60993bd61
ashakirzianov <ashakirzianov@icloud.com>
2019-08-10 18:57:27 -0700

    Remove parsing related code

--- src/preprocessBook/preprocess.ts
+++
@@ -1,9 +0,0 @@
-import { VolumeNode } from '../common/bookFormat';
-import { optimizeVolume } from './optimizeBook';
-import { simplifyVolume } from './simplifyBook';
-
-export function preprocessVolume(volume: VolumeNode): VolumeNode {
-    const simplified = simplifyVolume(volume);
-    const optimized = optimizeVolume(simplified);
-    return optimized;
-}
