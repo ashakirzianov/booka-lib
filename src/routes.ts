78cbd2bcaf325d60a3bbaf10afbd76ae60b73f5a
ashakirzianov <ashakirzianov@icloud.com>
2019-08-21 21:23:31 +0700

    Add 'back-utils'

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { books } from './db';
-import { LibContract, createRouter } from 'booka-common';
+import { LibContract } from 'booka-common';
+import { createRouter } from './back-utils';
 
 export const router = createRouter<LibContract>();
 
